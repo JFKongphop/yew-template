2fe0d7f65895462add90383d0b95b97d73cb6f0f
JFKongphop <kongphopleo@gmail.com>
2025-01-25 15:35:02 +0700

    add css

--- src/app.rs
+++ src/app.rs
@@ -2,11 +2,17 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
-    html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
-    }
+  html! {
+    <main>
+      <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+      <h1>{ "Hello World!" }</h1>
+      <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+      <div 
+        class="p-4 bg-red-500 text-blue-500"
+      >
+        { "Hello, Tailwind CSS with Yew!fbderggerg" }
+      </div>
+
+    </main>
+  }
 }
